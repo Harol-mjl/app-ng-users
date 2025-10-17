88474ce742afe25467d3829a78c08db71944e70e
Hjorge-l <haroljorgelopez@outlook.es>
2025-10-17 21:21:37 +0200

    feat: change scss to sass

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.sass'
 })
 export class HomeComponent {}
